9db2994446586a22a19b533c87a4c6f3279d18eb
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-07 14:23:00 +0100

    Ejercicios Tarea 3

---
+++ JavaModC1Tarea3/src/ClaseInteger1App.java
@@ -0,0 +1,26 @@
+
+public class ClaseInteger1App {
+public static void main(String[] args) {
+	    
+	    //Creacion de un objeto Integer usando el segundo constructor
+	    //Integer num1 = new Integer("125");
+	    Integer num1 = new Integer("125");
+	    
+	    //Creacion de un objeto Integer usando el primer constructor
+	    //Integer numero4=Integer.valueOf(20);
+	    Integer num2 = new Integer(20);
+	    
+	    //Obtención del entero que almacena cada objeto Integer.
+	    //Si no se hace esto, las lineas siguientes causarian error de compilación.
+	    int n1 = num1.intValue();
+	    int n2 = num2.intValue();
+	    System.out.println("Suma de " +n1+ " y " +n2+ " vale " +(n1+n2));
+	    if(n1+n2 > 130)
+	        n1++;
+	    else
+	        n1--;
+	    System.out.println(n1);
+	    
+	    }
+
+}
